b3edc115d013c991f30d947fe80cfd5b0456a74b
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-12-11 16:43:53 +0200

    fix: new logic for ingridient filtering

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,21 @@
 import User from "./User.js";
 import Testimonial from "./Testimonial.js";
 import Recipe from "./Recipe.js";
+import RecipeIngredient from "./RecipeIngredient.js";
+import Ingredient from "./Ingredient.js";
 
-User.hasMany(Testimonial, { foreignKey: "userId", onDelete: "CASCADE" });
-Testimonial.belongsTo(User, { foreignKey: "userId" });
-Recipe.belongsTo(User, { foreignKey: "userId" });
+User.hasMany(Testimonial, { foreignKey: "userid", onDelete: "CASCADE" });
+Testimonial.belongsTo(User, { foreignKey: "userid" });
+Recipe.belongsTo(User, { foreignKey: "userid" });
+Recipe.hasMany(RecipeIngredient, {
+  foreignKey: "recipeid",
+  sourceKey: "id",
+  as: "recipeIngredients",
+});
+RecipeIngredient.belongsTo(Ingredient, {
+  foreignKey: "ingredientid",
+  targetKey: "id",
+  as: "ingredient",
+});
 
-export { User, Testimonial, Recipe };
+export { User, Testimonial, Recipe, RecipeIngredient, Ingredient };
