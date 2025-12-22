ead05c122519526838f36be3d1ee1f972060b0fc
Inna Kuts <inna.v.kuts@gmail.com>
2025-12-22 15:57:10 +0100

    avatar-update-fix

--- services/userService.js
+++ services/userService.js
@@ -4,7 +4,7 @@ import Recipe from "../db/models/Recipe.js";
 import UserFollow from "../db/models/UserFollow.js";
 import HttpError from "../helpers/HttpError.js";
 import s3Client from "../uploader/s3-client.js";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { USER_AVATARS_S3_BUCKET_FOLDER } from "../helpers/constants.js";
 import randomImageName from "../helpers/generateRandomImageName.js";
 import sharp from "sharp";
@@ -230,10 +230,8 @@ const getUserRecipes = async (userId, page, limit) => {
 
 const updateCurrentUserAvatar = async (avatar, userId) => {
   const user = await getUserById(userId);
-  // If user.avatar exists, then we should update existing (the same name replaces existing on bucket) otherwise create a new avatar
-  const imageName =
-    user.avatar ??
-    `${USER_AVATARS_S3_BUCKET_FOLDER}/${userId}/${randomImageName()}`;
+  const currentAvatar = user.avatar;
+  const imageName = `${USER_AVATARS_S3_BUCKET_FOLDER}/${userId}/${randomImageName()}`;
   const imageBuffer = await sharp(avatar.buffer)
     .resize({ width: 120, height: 120, fit: "cover" })
     .toFormat("webp", { quality: 80 })
@@ -250,6 +248,18 @@ const updateCurrentUserAvatar = async (avatar, userId) => {
 
   await s3Client.send(putAvatarCommand);
   const userInfo = await user.update({ avatar: imageName });
+
+  if (currentAvatar) {
+    const deleteAvatarCommand = new DeleteObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: currentAvatar,
+    });
+    try {
+      await s3Client.send(deleteAvatarCommand);
+    } catch (error) {
+      // No-op: Ignore error
+    }
+  }
 
   return {
     avatar: await getSignedAvatarUrl(userInfo),
