98ba82680bd34553f7e4c85c00d01a48e03f8d96
Kostiantyn <kog@yousee.dk>
2025-12-22 16:15:53 +0200

    add order to areas

--- services/areaService.js
+++ services/areaService.js
@@ -1,7 +1,9 @@
 import Area from "../db/models/Area.js";
 
 const getAllAreas = async () => {
-  return await Area.findAll();
+  return await Area.findAll({
+    order: [["name", "ASC"]],
+  });
 };
 
 export default {
